420183b9d62a11f25767aee421092c9fb8fbb56f
colaman <colaman@nate.com>
2024-02-17 23:13:47 +0900

    최초 구성

---
+++ src/main/java/com/colamanlabs/springbatch/s0001/JenkinsSpringbatch0001Application.java
@@ -0,0 +1,13 @@
+package com.colamanlabs.springbatch.s0001;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsSpringbatch0001Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsSpringbatch0001Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/colamanlabs/springbatch/s0001/JenkinsSpringbatch0001ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.colamanlabs.springbatch.s0001;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsSpringbatch0001ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
